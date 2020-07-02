f464b5b3b9015aff8bd638faecff2e6fff8c2573
Wellad Silva <wellad.silva@hotmail.com>
2020-07-02 16:00:28 -0300

    CRUD Scaffolding

---
+++ ProjectSalesWebMvc/Data/ProjectSalesWebMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectSalesWebMvc.Models.Classe;
+
+namespace ProjectSalesWebMvc.Data
+{
+    public class ProjectSalesWebMvcContext : DbContext
+    {
+        public ProjectSalesWebMvcContext (DbContextOptions<ProjectSalesWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjectSalesWebMvc.Models.Classe.Department> Department { get; set; }
+    }
+}
--- ProjectSalesWebMvc/Startup.cs
+++ ProjectSalesWebMvc/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using ProjectSalesWebMvc.Data;
 
 namespace ProjectSalesWebMvc
 {
@@ -33,6 +35,9 @@ namespace ProjectSalesWebMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ProjectSalesWebMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ProjectSalesWebMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
