be8b7baa3740d4e3b06f81a4bda1329f67002d1e
Wellad Silva <wellad.silva@hotmail.com>
2020-07-03 22:30:30 -0300

    Simple Create form

--- ProjectSalesWebMvc/Services/SellerService.cs
+++ ProjectSalesWebMvc/Services/SellerService.cs
@@ -21,6 +21,10 @@ namespace ProjectSalesWebMvc.Services
             return _context.Seller.ToList();
         }
 
-
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
